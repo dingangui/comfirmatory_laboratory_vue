5578793321f12900020fe5101d7b5dfc9d0f2ee9
dingangui <771102120@qq.com>
2021-03-11 22:28:10 +0800

    saved on 3.11 on dell-lab

--- src/views/DetectionDataInput.vue
+++ src/views/DetectionDataInput.vue
@@ -5,16 +5,18 @@
         <el-container>
 
             <!-- 导航栏 -->
-            <el-header>Header</el-header>
+            <el-header>
+                <NavMenu></NavMenu>
+            </el-header>
 
             <!-- 唯一 div 内负责布局管理的，分为导航栏和主区域 -->
             <el-container>
 
                 <!-- 左侧，样品基本信息显示 -->
-                <el-col :span="12" >
+                <el-col :span="12" class="detection-data-input-sample-info">
 
                     <!-- 负责左侧布局管理的 div -->
-                    <div class="grid-content bg-purple detection-data-input-sample-info ">
+                    <div class="grid-content bg-purple">
 
                         <!-- 表单 快速填写样品基本信息 -->
                         <el-form :model="sampleBasicInfo" :rules="rules" ref="sampleBasicInfo">
@@ -37,7 +39,7 @@
                                 </el-col>
 
                                 <el-col :span="6">
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.inspectionDate"></el-input>
                                 </el-col>
 
                                 <!--第一行-->
@@ -48,15 +50,13 @@
                                     <div class="grid-content bg-purple">送检样品</div>
                                 </el-col>
                                 <el-col :span="6">
-                                    <div class="grid-content bg-purple-light">{{ sampleType }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.sampleType"></el-input>
                                 </el-col>
                                 <el-col :span="6">
                                     <div class="grid-content bg-purple">送检人群</div>
                                 </el-col>
                                 <el-col :span="6">
-                                    <div class="grid-content bg-purple-light">{{ inspectedType }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.inspectedType"></el-input>
                                 </el-col>
                             </el-row>
 
@@ -65,29 +65,25 @@
                                     <div class="grid-content bg-purple">姓名</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ name }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.name"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">性别</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ sex }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.sex"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">年龄</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ age }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.age"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">职业</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ profession }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.profession"></el-input>
                                 </el-col>
                             </el-row>
 
@@ -96,30 +92,69 @@
                                     <div class="grid-content bg-purple">国籍</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ country }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.country"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">民族</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <div class="grid-content bg-purple-light">{{ nation }}</div>
-                                    <el-input v-model="sampleBasicInfo.inspectionDate"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.nation"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">婚姻状况</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <el-input v-model="sampleBasicInfo.marriage"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.marriage"></el-input>
                                 </el-col>
                                 <el-col :span="3">
                                     <div class="grid-content bg-purple">文化程度</div>
                                 </el-col>
                                 <el-col :span="3">
-                                    <el-input v-model="sampleBasicInfo.educationalLevel"></el-input>
+                                    <el-input readonly v-model="sampleBasicInfo.educationalLevel"></el-input>
                                 </el-col>
                             </el-row>
 
+                            <el-row>
+                                <el-col :span="6">
+                                    <div class="grid-content bg-purple">身份证号</div>
+                                </el-col>
+                                <el-col :span="6">
+                                    <el-input readonly v-model="sampleBasicInfo.IDNumber"></el-input>
+                                </el-col>
+                                <el-col :span="6">
+                                    <div class="grid-content bg-purple">联系电话</div>
+                                </el-col>
+                                <el-col :span="6">
+                                    <el-input readonly v-model="sampleBasicInfo.phone"></el-input>
+                                </el-col>
+                            </el-row>
+
+                            <el-row>
+                                <el-col :span="6">
+                                    <div class="grid-content bg-purple">现住址</div>
+                                </el-col>
+                                <el-col :span="18">
+                                    <el-input readonly v-model="sampleBasicInfo.presentAddress"></el-input>
+                                </el-col>
+                            </el-row>
+
+                            <el-row>
+                                <el-col :span="6">
+                                    <div class="grid-content bg-purple">户籍地址</div>
+                                </el-col>
+                                <el-col :span="18">
+                                    <el-input readonly v-model="sampleBasicInfo.residenceAddress"></el-input>
+                                </el-col>
+                            </el-row>
+
+                            <el-row>
+                                <el-col :span="12">
+                                    <div class="grid-content bg-purple">本样品信息录入员</div>
+                                </el-col>
+                                <el-col :span="12">
+                                    <el-input readonly v-model="sampleBasicInfo.IDNumber"></el-input>
+                                </el-col>
+                            </el-row>
                             <!-- 表单 快速填写样品基本信息 -->
                         </el-form>
 
@@ -149,37 +184,37 @@
 </template>
 
 <script>
-    export default {
-        name: "DetectionDataInput",
-        data() {
-            return {
-                sampleBasicInfo: {
-                    inspectionUnit: ""
-                },
-                rules:{
+import NavMenu from "@/components/NavMenu";
 
-                }
-            }
-        },
-        created() {
-            const _this = this
-            this.$axios("/sample-basic-info/getSampleInfoByAcceptanceNumber/2021 - 7").then(res => {
-                    console.log(res.data.data)
-                    _this.sampleBasicInfo = res.data.data
-                }
-            )
+export default {
+    name: "DetectionDataInput",
+    components: {NavMenu},
+    data() {
+        return {
+            sampleBasicInfo: {
+                inspectionUnit: ""
+            },
+            rules: {}
         }
-
+    },
+    created() {
+        const _this = this
+        this.$axios("/sample-basic-info/getSampleInfoByAcceptanceNumber/2021 - 7").then(res => {
+                console.log(res.data.data)
+                _this.sampleBasicInfo = res.data.data
+                _this.activeIndex = '3'
+            }
+        )
     }
+
+}
 </script>
 
 <style scoped>
-    .el-col {
-        border: solid 1px #000;
-        height: 50px;
-    }
 
-    .detection-data-input-sample-info .el-input{
-    }
+
+.el-input__inner {
+    padding: 0;
+}
 
 </style>
